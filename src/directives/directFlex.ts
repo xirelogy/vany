3021a3870a6c459a4f5a70f5efe9a14286eadb9f
jxtou <jxtou@xirelogy.com>
2024-11-10 22:22:00 +0800

    Improve 'v-flex'

--- src/directives/directFlex.ts
+++ src/directives/directFlex.ts
@@ -7,6 +7,25 @@ import { type VanyCompatDirective } from '.';
 import VanyCommonActuatorHandle from '../setup/VanyCommonActuatorHandle';
 
 
+/**
+ * Process directive
+ * @param el Element
+ * @param binding Binding
+ */
+function process(el: any, binding: DirectiveBinding<void>) {
+  const _isHor = binding.modifiers?.hor === true;
+  const _isVer = binding.modifiers?.ver === true;
+  const _isFixed = binding.modifiers?.fixed === true;
+  const _isSpring = binding.modifiers?.spring === true;
+
+  if (el instanceof HTMLElement) {
+    if (_isHor) el.classList.add('vany-std-flex-hor');
+    if (_isVer) el.classList.add('vany-std-flex-ver');
+    if (_isFixed) el.classList.add('vany-std-flex-item-fixed');
+    if (_isSpring) el.classList.add('vany-std-flex-item-spring');
+  }
+}
+
 
 /**
  * Directive provider
@@ -14,22 +33,14 @@ import VanyCommonActuatorHandle from '../setup/VanyCommonActuatorHandle';
  */
 function directFlex(): VanyCompatDirective|undefined {
   return {
-    beforeMount(el: any, binding: DirectiveBinding<void>) {
-      const _isHor = binding.modifiers?.hor === true;
-      const _isVer = binding.modifiers?.ver === true;
-      const _isFixed = binding.modifiers?.fixed === true;
-      const _isSpring = binding.modifiers?.spring === true;
-
-      if (el instanceof HTMLElement) {
-        if (_isHor) el.classList.add('vany-std-flex-hor');
-        if (_isVer) el.classList.add('vany-std-flex-ver');
-        if (_isFixed) el.classList.add('vany-std-flex-item-fixed');
-        if (_isSpring) el.classList.add('vany-std-flex-item-spring');
-      }
+    mounted(el: any, binding: DirectiveBinding<void>) {
+      process(el, binding);
+    },
+    updated(el: any, binding: DirectiveBinding<void>) {
+      process(el, binding);
     },
   };
 }
-
 
 
 /**
